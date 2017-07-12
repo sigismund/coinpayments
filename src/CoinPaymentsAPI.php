d66a1fa48b5e7fa20e2b30c0fc5aaed834e8f710
Žiga Drnovšček <web@sigismund.si>
2017-07-12 10:44:11 +0200

    Old commit

--- src/CoinPaymentsAPI.php
+++ src/CoinPaymentsAPI.php
@@ -3,13 +3,18 @@ namespace Sigismund\CoinPayments;
 
 class CoinPaymentsAPI
 {
-    private $private_key = '';
+    private $merchantID = '';
+    private $ipnSecret = '';
+    private $privateKey = '';
     private $public_key = '';
     private $ch = null;
 
-    public function setup($private_key, $public_key) {
-        $this->private_key = $private_key;
-        $this->public_key = $public_key;
+    public function __construct($privateKey, $public_key, $merchantID, $ipnSecret)
+    {
+        $this->ipnSecret = $ipnSecret;
+        $this->merchantID = $merchantID;
+        $this->privateKey = $privateKey;
+        $this->publicKey = $public_key;
         $this->ch = null;
     }
 
@@ -17,11 +22,9 @@ class CoinPaymentsAPI
      * Gets the current CoinPayments.net exchange rate. Output includes both crypto and fiat currencies.
      * @param short If short == true (the default), the output won't include the currency names and confirms needed to save bandwidth.
      */
-    public function getRates($short = true, $accepted = true)
+    public function getRates(bool $short = true, bool $accepted = true)
     {
-        $short = $short ? 1:0;
-        $accepted = $accepted ? 1:0;
-        return $this->apiCall('rates', [(int) 'short' => $short, (int) 'accepted' => $accepted]); 
+        return $this->apiCall('rates', ['short' => (int) $short, 'accepted' => (int) $accepted]);
     }
 
     /**
@@ -39,27 +42,50 @@ class CoinPaymentsAPI
      * @param amount The amount of the transaction (floating point to 8 decimals).
      * @param currency1 The source currency (ie. USD), this is used to calculate the exchange rate for you.
      * @param currency2 The cryptocurrency of the transaction. currency1 and currency2 can be the same if you don't want any exchange rate conversion.
-     * @param address Optionally set the payout address of the transaction. If address is empty then it will follow your payout settings for that coin.
-     * @param ipn_url Optionally set an IPN handler to receive notices about this transaction. If ipn_url is empty then it will use the default IPN URL in your account.
-     * @param buyer_email Optionally (recommended) set the buyer's email so they can automatically claim refunds if there is an issue with their payment.
+     * @param additional Optionally set additional fields.
      */
-    public function createTransactionSimple($amount, $currency1, $currency2, $address='', $ipnUrl='', $buyerEmail='')
+    public function createTransactionSimple($amount, $currencyIn, $currencyOut, $additional = [])
     {
-        $req = array(
+        $acceptableFields = ['amount', 'currency1', 'currency2', 'address', 'buyer_email', 'buyer_name',
+            'item_name', 'item_number', 'invoice', 'custom', 'ipn_url'];
+
+        $request = [
             'amount' => $amount,
-            'currency1' => $currency1,
-            'currency2' => $currency2,
-            'address' => $address,
-            'ipn_url' => $ipnUrl,
-            'buyer_email' => $buyerEmail,
-        );
-        return $this->apiCall('create_transaction', $req);
+            'currency1' => $currencyIn,
+            'currency2' => $currencyOut,
+        ];
+
+        foreach ($acceptableFields as $field) {
+            if (isset($additional[$field])) {
+                $req[$field] = $additional[$field];
+            }
+        }
+
+        return $this->apiCall('create_transaction', $request);
     }
 
     public function createTransaction($req)
     {
         // See https://www.coinpayments.net/apidoc-create-transaction for parameters
         return $this->apiCall('create_transaction', $req);
+    }
+
+    /**
+     * Get transaction information via transaction ID
+     *
+     * @param string $txID
+     * @param bool $all
+     * @return void
+     */
+    public function getTransactionInfo($txID, $all = true)
+    {
+        $req = array(
+            'txid' => $txID,
+            'full' => (int) $all
+
+        );
+
+        return $this->apiCall('get_tx_info', $req);
     }
 
     /**
@@ -84,13 +110,13 @@ class CoinPaymentsAPI
      * @param auto_confirm If auto_confirm is true, then the withdrawal will be performed without an email confirmation.
      * @param ipn_url Optionally set an IPN handler to receive notices about this transaction. If ipn_url is empty then it will use the default IPN URL in your account.
      */
-    public function createWithdrawal($amount, $currency, $address, $auto_confirm = false, $ipnUrl = '')
+    public function createWithdrawal($amount, $currency, $address, $autoConfirm = false, $ipnUrl = '')
     {
         $req = array(
             'amount' => $amount,
             'currency' => $currency,
             'address' => $address,
-            'auto_confirm' => $auto_confirm ? 1:0,
+            'auto_confirm' => $autoConfirm ? 1:0,
             'ipn_url' => $ipnUrl,
         );
         return $this->apiCall('create_withdrawal', $req);
@@ -103,13 +129,13 @@ class CoinPaymentsAPI
      * @param merchant The merchant ID to send the coins to.
      * @param auto_confirm If auto_confirm is true, then the transfer will be performed without an email confirmation.
      */
-    public function createTransfer($amount, $currency, $merchant, $auto_confirm = false)
+    public function createTransfer($amount, $currency, $merchant, $autoConfirm = false)
     {
         $req = array(
             'amount' => $amount,
             'currency' => $currency,
             'merchant' => $merchant,
-            'auto_confirm' => $auto_confirm ? 1:0,
+            'auto_confirm' => $autoConfirm ? 1:0,
         );
         return $this->apiCall('create_transfer', $req);
     }
@@ -121,20 +147,62 @@ class CoinPaymentsAPI
      * @param pbntag The $PayByName tag to send funds to.
      * @param auto_confirm If auto_confirm is true, then the transfer will be performed without an email confirmation.
      */
-    public function sendToPayByName($amount, $currency, $pbntag, $auto_confirm = false)
+    public function sendToPayByName($amount, $currency, $pbntag, $autoConfirm = false)
     {
         $req = array(
             'amount' => $amount,
             'currency' => $currency,
             'pbntag' => $pbntag,
-            'auto_confirm' => $auto_confirm ? 1:0,
+            'auto_confirm' => $autoConfirm ? 1:0,
         );
         return $this->apiCall('create_transfer', $req);
     }
 
+    /**
+     * Validate the IPN request and payment.
+     *
+     * @param  array  $postData
+     * @param  array  $serverData
+     * @return mixed
+     */
+    public function validate(array $postData, array $serverData)
+    {
+        if (!isset($postData['ipn_mode'], $postData['merchant'], $postData['status'], $postData['status_text'])) {
+            throw new \Exception("Insufficient POST data provided.");
+        }
+        if ($postData['ipn_mode'] == 'httpauth') {
+            if ($serverData['PHP_AUTH_USER'] !== $this->merchantID) {
+                throw new \Exception("Invalid merchant ID provided.");
+            }
+
+            if ($serverData['PHP_AUTH_PW'] !== $this->ipnSecret) {
+                throw new \Exception("Invalid IPN secret provided.");
+            }
+        } elseif ($postData['ipn_mode'] == 'hmac') {
+            $hmac = hash_hmac("sha512", file_get_contents('php://input'), $this->ipnSecret);
+
+            if ($hmac !== $serverData['HTTP_HMAC']) {
+                throw new \Exception("Invalid HMAC provided.");
+            }
+            if ($postData['merchant'] !== $this->merchantID) {
+                throw new \Exception("Invalid merchant ID provided.");
+            }
+        } else {
+            throw new \Exception("Invalid IPN mode provided.");
+        }
+        $order_status = $postData['status'];
+        $order_status_text = $postData['status_text'];
+        if ($order_status < 0) {
+            throw new \Exception("{$order_status}: {$order_status_text}");
+        } elseif ($order_status >= 0 && $order_status < 100 && $order_status != 2) {
+            return false;
+        }
+        return true; // If $order_status is >100 or is 2, return true
+    }
+
     private function isSetup()
     {
-        return (!empty($this->private_key) && !empty($this->public_key));
+        return (!empty($this->privateKey) && !empty($this->publicKey));
     }
 
     private function apiCall($cmd, $req = array())
@@ -146,14 +214,14 @@ class CoinPaymentsAPI
         // Set the API command and required fields
         $req['version'] = 1;
         $req['cmd'] = $cmd;
-        $req['key'] = $this->public_key;
+        $req['key'] = $this->publicKey;
         $req['format'] = 'json'; //supported values are json and xml
 
         // Generate the query string
-        $post_data = http_build_query($req, '', '&');
+        $postData = http_build_query($req, '', '&');
 
         // Calculate the HMAC signature on the POST data
-        $hmac = hash_hmac('sha512', $post_data, $this->private_key);
+        $hmac = hash_hmac('sha512', $postData, $this->privateKey);
 
         // Create cURL handle and initialize (if needed)
         if ($this->ch === null) {
@@ -163,7 +231,7 @@ class CoinPaymentsAPI
             curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, 0);
         }
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, array('HMAC: '.$hmac));
-        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postData);
 
         $data = curl_exec($this->ch);
         if ($data !== false) {
